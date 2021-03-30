76937230a36b17d524ce46db5374a837862a0abb
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-30 21:14:19 +0300

    step1

---
+++ index.js
@@ -0,0 +1,31 @@
+// import express (after npm install express)
+const express = require('express');
+
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+const PORT = 3000;
+
+// create a route for the app
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+
+// // create a route for the test
+// app.get('/test', (req, res) => {
+//   res.send('{status:200, message:"ok"}');
+// });
+
+// // // create a route for the time
+// // var time=new Date(hours,minutes);
+// // app.get('/time', (req, res) => {
+// //   res.send('{status:200, message:<'+time+'>}');
+// // });
+
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
