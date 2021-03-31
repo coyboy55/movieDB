0ca3153e8b9f78afb8dcf18ed9bfd4e33aae1fa6
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-31 17:34:25 +0300

    step4

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 // create new express app and save it as "app"
 const app = express();
 
+// create new express app and save it as "router"
+const router = express.Router();
+
+
 // server configuration
 const PORT = 3000;
 
@@ -18,6 +22,26 @@ app.get('/test', (req, res) => {
   res.send('{status:200, message:"ok"}');
 });
 
+// create a route for the hello/<ID>
+
+
+app.get('/hello/:id', (req, res) => {
+  res.send('{status:200, message:hello,'+req.params.id+'}');
+});
+// create a route for the hello/ID=""
+app.get('/hello', (req, res) => {
+  res.send('{status:200, message:hello,}');
+});
+
+// create a route for the search
+app.get('/search', (req, res) => {
+    if (req.query.s){
+      res.send('{status:200, message:"ok", data:'+req.query.s.toString()+'}');
+  }else{
+    res.send('{status:500, error:true, message:"you have to provide a search"}');
+  }
+
+});
 // create a route for the time
 var time= new Date(Date.now());
 app.get('/time', (req, res) => {
